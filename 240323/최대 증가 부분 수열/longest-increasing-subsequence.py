a895bd5e2e1a3889134abd677313596043a3dd1d
Minjoo <62490238+mingzooo@users.noreply.github.com>
2024-03-23 00:07:09 +0900

    Title: 최대 증가 부분 수열, Time: 145ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240323/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 240323/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -5,7 +5,7 @@ arr = list(map(int, input().split()))
 dp = [0 for _ in range(n)]
 dp[0] = 1
 
-for i in range(1, n):
+for i in range(n):
     for j in range(i):
         if (arr[j] < arr[i]):
             dp[i] = max(dp[i], dp[j]+1)
